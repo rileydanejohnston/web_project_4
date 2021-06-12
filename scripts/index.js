51d10f2ac1f9cee066a5f372770d1379cceb385c
Riley <rileydanejohnston@gmail.com>
2021-06-12 10:25:11 -0700

    feat: like button activates when clicked

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,10 @@
-let myObject = document.querySelector('.root');
+let likeBtn = document.querySelectorAll('.cards__like-button');
 
-console.log(myObject.className);
+for (let i = 0; i < likeBtn.length; ++i)
+{
+  likeBtn[i].addEventListener("click", function() {
+    likeBtn[i].classList.toggle('cards__like-button_active');
+  });
+}
+
+/* I can select the first like button and make it active with querySelector. I couldn't figure out how to select all and get the index of the like button that was clicked. I got help from this post: https://stackoverflow.com/questions/50643302/addeventlistener-on-a-queryselectorall-with-classlist */
