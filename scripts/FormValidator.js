caa20f97cd14c30492ef44cf2072376f6ba3f40c
Riley <rileydanejohnston@gmail.com>
2021-08-02 12:31:57 -0700

    fix: create allInputs field, replace all params/occurances with field

--- scripts/FormValidator.js
+++ scripts/FormValidator.js
@@ -16,8 +16,8 @@ export default class FormValidator {
     this._submit.setAttribute('disabled', true);
   }
 
-  _toggleButton(inputs) {
-    const valid = inputs.every(input => input.validity.valid === true);
+  _toggleButton() {
+    const valid = this._allInputs.every(input => input.validity.valid === true);
 
     if (valid){
       this._submit.classList.remove(this._inactiveButtonClass);
@@ -52,21 +52,21 @@ export default class FormValidator {
     }
   }
 
-  _setListeners(allInputs, input) {
+  _setListeners(input) {
 
 
     input.addEventListener('input', () => {
       this._checkValidity(input);
-      this._toggleButton(allInputs);
+      this._toggleButton();
     });
   }
 
   enableValidation() {
-    const allInputs = Array.from(this._form.querySelectorAll(this._inputSelector));
+    this._allInputs = Array.from(this._form.querySelectorAll(this._inputSelector));
 
 
-    allInputs.forEach(input => {
-      this._setListeners(allInputs, input);
+    this._allInputs.forEach(input => {
+      this._setListeners(input);
     });
   }
 }
