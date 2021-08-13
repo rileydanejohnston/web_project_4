90ec0b989ddc483c6d1554b1016e6601ff014bb1
Riley <rileydanejohnston@gmail.com>
2021-08-13 15:24:04 -0700

    feat: create Section class

--- page/index.js
+++ page/index.js
@@ -1,5 +1,6 @@
 import Card from '../components/Card.js';
 import FormValidator from '../components/FormValidator.js';
+import Section from '../components/Section.js';
 
 const settings = {
   formSelector: ".popup__form",
