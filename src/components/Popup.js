41cb411e934c42000b398b2e58f313d8f1344b77
Riley <rileydanejohnston@gmail.com>
2021-08-19 20:50:47 -0700

    fix: remove esc event listener in Popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,14 +1,15 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popup = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
   open() {
     this._popup.classList.add('popup_active');
-    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    document.addEventListener('keydown', this._handleEscClose);
   }
   close() {
     this._popup.classList.remove('popup_active');
-    document.removeEventListener('keydown', this._handleEscClose.bind(this));
+    document.removeEventListener('keydown', this._handleEscClose);
   }
   _handleEscClose(e) {
     if (e.key === 'Escape'){
